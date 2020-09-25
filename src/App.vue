481736e5966f6e22010b04cac97ab433c2ed0077
YiJio <841312998@qq.com>
2020-09-25 09:47:28 +0800

    使用toRef   使用toRefs 解构 reactive

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,14 @@
   <input type="text" v-model="vModelName">
   <vModel v-model="vModelName"></vModel>
   <ButtonMessage></ButtonMessage>
+  <br>
+  <input type="text" v-model="xing">
+  <input type="text" v-model="ming">
+  <p>姓名是: {{ xingMing }}---{{ test }}---{{ test2 }}</p>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed, reactive, toRefs } from 'vue'
 import editTitle from './editTitle'
 import Timer from './components/timer.vue'
 import vModel from './components/vModel.vue'
@@ -20,8 +24,18 @@ export default {
   },
   setup() {
     let { name, userInput, edit } = editTitle()
-    let vModelName = ref('v-model')
-    return { name, userInput, edit, vModelName }
+    // let vModelName = ref('v-model')
+    // let xing = ref('')
+    // let ming = ref('')
+    let data = reactive({
+      vModelName: 'v-model',
+      xing: '',
+      ming: '',
+      test: '干活啦！',
+      test2: '搬砖吧！'
+    })
+    let xingMing = computed(() => data.xing + data.ming)
+    return { name, userInput, edit, xingMing, ...toRefs(data) }
   }
 }
 </script>
