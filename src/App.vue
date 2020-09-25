0489b64bafaad182b613cfd38f15efc88ae39897
YiJio <841312998@qq.com>
2020-09-25 20:17:10 +0800

    异步组件 defineAsyncComponent

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,23 @@
   <input type="text" v-model="xing">
   <input type="text" v-model="ming">
   <p>姓名是: {{ xingMing }}---{{ test }}---{{ test2 }}</p>
+  <br>
+  <button @click="asyncComHidden = false">切换展示异步组件</button>
+  <asyncCom v-if="!asyncComHidden"></asyncCom>
 </template>
 
 <script>
-import { ref, computed, reactive, toRefs } from 'vue'
+import { ref, computed, reactive, toRefs, defineAsyncComponent } from 'vue'
 import editTitle from './editTitle'
 import Timer from './components/timer.vue'
 import vModel from './components/vModel.vue'
+const asyncCom = defineAsyncComponent(() => import('./components/async.vue'))
 export default {
   name: 'App',
   components: {
     Timer,
-    vModel
+    vModel,
+    asyncCom
   },
   setup() {
     let { name, userInput, edit } = editTitle()
@@ -32,7 +37,8 @@ export default {
       xing: '',
       ming: '',
       test: '干活啦！',
-      test2: '搬砖吧！'
+      test2: '搬砖吧！',
+      asyncComHidden: true
     })
     let xingMing = computed(() => data.xing + data.ming)
     return { name, userInput, edit, xingMing, ...toRefs(data) }
---
+++ src/components/async.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="async">
+    这是一个异步组件
+  </div>
+</template>
+
+<style scoped>
+.async {
+  width: 200px;
+  height: 80px;
+  border: 1px solid red;
+}
+</style>
